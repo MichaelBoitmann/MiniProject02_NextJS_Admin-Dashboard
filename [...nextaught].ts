08b2b1c90501af3b12255ac90c2dee1819d08f84
Michael <michael.boitmann@gmail.com>
2024-08-20 22:47:46 +1200

    updated .env authkeys and secret keys and nextauth

---
+++ [...nextaught].ts
@@ -0,0 +1,4 @@
+import prisma from '@/lib/prisma'
+import { PrismaAdapter }from 'next-auth/prisma-adpater'
+import NextAuth, { AuthOptions } from 'next-auth
+import Github from 'next-auth/'providers/dubh
